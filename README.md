265a4b1798028f11b7ee9eb0e0846d6751db7899
Bon Crowder <SilverDash@Bons-MacBook-Pro.local>
2017-05-23 17:21:28 -0500

    turns out I had an issue with connection, but now it's fixed; also updated readme

--- README.md
+++ README.md
@@ -1,9 +1,46 @@
-# Buy from Bon
-An amazon-like app for buying stuff (homework 10)
+# Buy, by Bon
 
-This is a node.js application that needs to be run from the command line.
- 
- You can find the source files at [https://github.com/mathfour/buy-from-bon](https://github.com/mathfour/buy-from-bon)
+*Buy, by Bon* is a Node.js and SQL command line app with an Amazon-like storefront and separate customer, manager and executive facing applications. The app uses a database of products and takes in orders from customers, which depletes stock from the store's inventory. Managers and executives can add more products to different departments and track profit.
+
+ You can find the source files at [https://github.com/mathfour/buy-by-bon](https://github.com/mathfour/buy-by-bon)
+
+## Dependencies
+
+Clone this repo to your desktop and run `npm install` to install its 4 dependencies.
+
+- [mySQL](https://www.npmjs.com/package/mysql): A node.js driver for mysql
+
+- [Colors](https://www.npmjs.com/package/colors): Used to generate colored text in the console 
+
+- [Inquirer](https://www.npmjs.com/package/inquirer): Used to user input
+
+- [Console.table](https://www.npmjs.com/package/console.table): A method that prints an array of objects as a table in console
+
+## Set Up
+Once you are in the *Buy, by Bon* repository, run the following code in the command line to create the *Buy, by Bon* database and the Products and Departments tables.
+
+- `mysql -u root`
+
+- `source products.sql`
+
+- `source departments.sql`
+
+-  `exit`
+
+Then use your text editor of choice to update buy-by-bon.js ~~, manager.js, and exec.js~~ with your mysql password, if you use one.
+
+# Customer Use
+
+- Run `node buy-by-bon.js` to start the customer facing *Buy, by Bon* app
+
+- Customers are shown the catalog and may choose a product to purchase by its ID. If a product has no inventory, it **_will_** be shown in the catalog, but **_will not_** come up as an option to purchase.
+
+- Customers are shown the total and are asked to confirm their purchase. After the purchase the catalog (via the mysql database) is updated to reflect new stock quantities.
+
+- Customers are then prompted to either exit or make another purchase.
 
  
- 
+### Acknowledgements 
+
+A big thanks to the following folks:
+- [Stephanie Orpilla](https://github.com/stephorpilla) whose `README.md` helped me create this one.
--- buy-by-bon.js
+++ buy-by-bon.js
@@ -5,8 +5,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("console.table");
-
-// var Table = require('cli-table'); // bmc: constructor
+var colors = require("colors");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,38 +14,38 @@ var connection = mysql.createConnection({
     password: "",
     database: "buy_by_bon"
 });
-
+connection.connect(function (err) {
+    if (err) {
+        console.log(err);
+    }
+});
 function showCatalog(cbPrompt) {
-    console.log("----------------------------");
-    console.log("Welcome to Buy, by Bon")
-    console.log("----------------------------");
-    console.log("Here is our current catalog:");
-    connection.connect(function (err) {
+    console.log("----------------------------".rainbow);
+    console.log("Welcome to Buy, by Bon".blue);
+    console.log("----------------------------".rainbow);
+    console.log("Here is our current catalog:".green);
+
+    connection.query("SELECT id, name, price FROM products;", function (err, res) {
         if (err) {
             console.log(err);
         }
-
-        connection.query("SELECT id, name, price FROM products;", function (err, res) {
+        else {
             console.table(res);
-            // var catalog = new Table();
-            // catalog.push(res);
-            // console.log(catalog);
-        });
-        connection.query("SELECT * FROM products WHERE quantity > 0;", function (err, res) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                JSON.stringify(res);
-                var itemsToShow = [];
-                for (var i = 0; i < res.length; i++) {
-                    itemsToShow.push(res[i].id);
-                }
-                cbPrompt(itemsToShow);
-            }
-
-        })
+        }
     });
+    connection.query("SELECT * FROM products WHERE quantity > 0;", function (err, res) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            JSON.stringify(res);
+            var itemsToShow = [];
+            for (var i = 0; i < res.length; i++) {
+                itemsToShow.push(res[i].id);
+            }
+            cbPrompt(itemsToShow);
+        }
+    })
 }
 
 function prompt(items) {
@@ -54,7 +53,7 @@ function prompt(items) {
         {
             type: "list",
             name: "item",
-            message: "Which item would you like to purchase? \n(If the item number is not shown, it's because \n we currently have no inventory.)",
+            message: "Which item would you like to purchase? \n(If the item number is not shown, it's because \n we currently have no inventory.)".blue,
             choices: items
         },
         {
@@ -69,12 +68,12 @@ function prompt(items) {
             }
             else {
                 if (res.length > 0) {
-                    console.log("We are happy to complete your order!");
+                    console.log("We are happy to complete your order!".rainbow);
                     sellItem(a.item, a.quantity);
                 }
                 else {
-                    console.log("Unfortunately we do not have sufficient inventory to complete" +
-                            " your order");
+                    console.log("Unfortunately we do not have sufficient inventory to complete".green +
+                            " your order".green);
                     startOver();
                 }
             }
@@ -90,20 +89,20 @@ function sellItem(itemID, qty) {
         else {
             var totalPrice = parseFloat(res[0].price) * parseFloat(qty);
             console.log("Your total is $", totalPrice);
-            console.log("Send a check or money order to 1123 Fibonacci Lane, Houston, Texas");
-            console.log("Allow 4-6 weeks for delivery");
+            console.log("Send a check or money order to 1123 Fibonacci Lane, Houston, Texas".blue);
+            console.log("Allow 4-6 weeks for delivery".blue);
             // bmc: confirm purchase?
             inquirer.prompt({
                 type: "confirm",
-                message: "Would you like to complete this order?",
+                message: "Would you like to complete this order?".green,
                 name: "confirmOrder",
                 default: "yes"
             }).then(function (a) {
                 if (a.confirmOrder === false) {
-                    console.log("Your order has been cancelled.");
+                    console.log("Your order has been cancelled.".red);
                 }
                 else {
-                    console.log("Thank you for your order!");
+                    console.log("Thank you for your order!".rainbow);
                     connection.query("SELECT quantity FROM products WHERE id=?", itemID, function (err, res) {
                         if (err) {
                             console.log(err);
@@ -128,7 +127,7 @@ function sellItem(itemID, qty) {
 function startOver() {
     inquirer.prompt({
         type: "list",
-        message: "Would you like to order something else or quit?",
+        message: "Would you like to order something else or quit?".blue,
         name: "action",
         choices: ["Order", "Quit"]
     }).then(function (a) {
@@ -142,3 +141,4 @@ function startOver() {
 }
 
 showCatalog(prompt);
+
